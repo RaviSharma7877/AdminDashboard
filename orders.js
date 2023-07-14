4a98fb8296272d3c34e6e17f14be863b649a4b2d
Ravi Sharma <sraa7877@gmail.com>
2023-07-14 21:35:04 +0530

    done

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName : 'Tomato',
+        productNumber: '00001',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName : 'Potato',
+        productNumber: '00002',
+        paymentStatus: 'Refunded',
+        status: 'Decline'
+    },
+    {
+        productName : 'Chilli',
+        productNumber: '00003',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+]
